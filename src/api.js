6d51df8b6792e955b6451dcec51c8daab731c05f
Emma Ågren <agren.gastronomi@hotmail.com>
2026-03-17 10:16:09 +0100

    Lägger in api för väder.

---
+++ src/api.js
@@ -0,0 +1,16 @@
+/**
+ * Hämtar väderdata från Open-Meteo för specifik plats.
+ * @param {number} lat - latitud
+ * @param {number} lon - longitud
+ * @returns {Promise<Object} Aktuellt väder
+ */
+
+export async function fetchWeather(lat, lon) {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
+    const response = await fetch(url);
+
+    if (!response.ok) throw new Error ('Kunde inte hämta väderdata');
+
+    const data = await response.json();
+    return data.current_weather;
+}
